7c7a4fa7c8bd09be8360767b7beae959ac66c335
lazyreport <kinghyejoo@gmail.com>
2022-04-12 00:34:10 +0900

    header, crypto context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Header from './Components/Header';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import CoinPage from "./Pages/CoinPage";
+import Homepage from "./Pages/Homepage";
+import Box from "@mui/material/Box";
+/* import { styled } from '@mui/material/styles'; */
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-    <Header />
-    </div>
+      <Box sx={{ bgcolor: "#14161a", color: "#fff", minHeight: "100vh" }}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </Box>
     </BrowserRouter>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import { ThemeProvider } from "@emotion/react";
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
 
 const Header = () => {
+  let navigate = useNavigate();
+  let { currency, symbol, setCurrency } = CryptoState();
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      mode: "dark",
+    },
+  });
   return (
-    <div>Header</div>
-  )
-}
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              onClick={() => navigate("/")}
+              sx={{
+                flex: 1,
+                color: "gold",
+                fontFamily: "Montserrat",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+            >
+              Crypto Tracker
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{ width: 100, height: 40, marginRight: 15 }}
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"KRW"}>KRW</MenuItem>
+              <MenuItem value={"USD"}>USD</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
 
-export default Header
+export default Header;
